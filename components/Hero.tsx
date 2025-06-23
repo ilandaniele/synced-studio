c70b402136681dfc28313ba559a564ea81f8bb82
Ilan Daniele <ilan.daniele@gmail.com>
2025-06-23 11:44:45 -0300

    adding changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,8 +9,8 @@ const Hero: React.FC = () => {
         <Image
           src="/images/synced-choco.jpg"
           alt="Synced Chocolate"
-          width={1920}          // ancho original de tu asset
-          height={1280}         // alto original de tu asset
+          width={4746}          // ancho original de tu asset
+          height={2855}         // alto original de tu asset
           quality={100}         // máxima calidad
           className="w-full h-auto object-contain"
           priority
