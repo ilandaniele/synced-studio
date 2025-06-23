a4a873b07002e08c6398460d739460c43e29bb6f
Ilan Daniele <ilan.daniele@gmail.com>
2025-06-23 11:03:39 -0300

    adding changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,18 +4,19 @@ import React from 'react';
 
 const Hero: React.FC = () => {
   return (
-    <section className="relative w-full h-screen overflow-hidden">
-      <div className="absolute inset-0">
+    <section className="relative w-screen overflow-visible">
+      <div className="w-screen">
         <Image
           src="/images/synced-choco.jpg"
           alt="Synced Chocolate"
-          fill
-          className="object-cover"
+          width={1920}          // ancho original de tu asset
+          height={1280}         // alto original de tu asset
+          quality={100}         // máxima calidad
+          className="w-full h-auto object-contain"
           priority
         />
       </div>
-    </section> 
-    // <section className="bg-[#160d09] relative w-full h-screen overflow-hidden">
+    </section>
   );
 };
 
